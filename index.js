54997920f87db8fa3ab0ff0be218e7f917b00044
ISIOMA123123 <ubanisuccess26@gmail.com>
2024-12-16 11:59:08 +0100

    added the functionality using the api

--- index.js
+++ index.js
@@ -10,5 +10,27 @@ searchFormIdentity.addEventListener("submit", function seachListener(event) {
 
     cityElementResult = document.querySelector("#cityElement");
     cityElementResult.innerHTML = searchFormInputResult.value;
+
+
+
+let city = searchFormInputResult.value;
+console.log(city);
+let apiKey = "7d3fo7t42ac4a88b5f1b6383fbbe2b0b";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+
+axios.get(apiUrl).then(function searchingCity(response) {
+ console.log(response); 
+ 
+ temperatureNow = Math.round(response.data.temperature.current);
+ console.log(temperatureNow);
+
+ presentTemperature = document.querySelector("#temperatureValuenow");
+ console.log(presentTemperature);
+
+ presentTemperature.innerHTML = `${temperatureNow}`;
+});
+
+
 }
 );
