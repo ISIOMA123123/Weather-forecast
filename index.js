172078fa6a79b36b2146064df83f9bbcd9b6b463
ISIOMA123123 <ubanisuccess26@gmail.com>
2024-12-21 16:23:02 +0100

    i updated the functions

--- index.js
+++ index.js
@@ -1,62 +1,75 @@
-searchFormIdentity = document.querySelector("#seachForm-identity");
-console.log(searchFormIdentity);
+function updateWeather(response) {
+    console.log(response.data);
 
-searchFormIdentity.addEventListener("submit", function seachListener(event) {
+     temperatureElement = document.querySelector("#temperatureValuenow");
+    temperature = response.data.temperature.current;
+   temperatureElement.innerHTML = Math.round(temperature);
 
-    event.preventDefault();
+   cityElementHone = document.querySelector("#cityElement");
+    cityElementHone.innerHTML = response.data.city;
+    
+   descriptionInformation = response.data.condition.description;
+    descriptionElement = document.querySelector("#weatherDescription");
+    descriptionElement.innerHTML = descriptionInformation;
+
+    humidityElementInformation = response.data.temperature.humidity;
+    humidityElement = document.querySelector("#humidityResult");
+    humidityElement.innerHTML = `${humidityElementInformation}%`
+
+    windInformation = response.data.wind.speed;
+    windElement = document.querySelector("#windDescription");
+    windElement.innerHTML = `${windInformation}km/hr`
    
-    searchFormInputResult = document.querySelector("#searchFormInputes");
-    console.log(searchFormInputResult.value);
+    date = new Date(response.data.time * 1000);
+    timeElement = document.querySelector("#weatherTimeNow");
+    timeElement.innerHTML = formatDate(date);
 
-    cityElementResult = document.querySelector("#cityElement");
-    cityElementResult.innerHTML = searchFormInputResult.value;
-
-
-
-let city = searchFormInputResult.value;
-console.log(city);
-let apiKey = "7d3fo7t42ac4a88b5f1b6383fbbe2b0b";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-
-axios.get(apiUrl).then(function searchingCity(response) {
- console.log(response); 
- 
- temperatureNow = Math.round(response.data.temperature.current);
- console.log(temperatureNow);
- descriptionNow = response.data.condition.description;
- console.log(descriptionNow);
-
- humidityNow = response.data.temperature.humidity;
-console.log(humidityNow);
-
-windNow = response.data.wind.speed;
-console.log(windNow);
-
-date = new Date(response.data.time * 1000);
-console.log(date);
-
- presentTemperature = document.querySelector("#temperatureValuenow");
- console.log(presentTemperature);
-
- presentTemperature.innerHTML = `${temperatureNow}`;
-
- weatherDescriptionNow = document.querySelector("#weatherDescription");
- console.log(weatherDescriptionNow);
-
- weatherDescriptionNow.innerHTML = descriptionNow;
-
- humidityResultnow = document.querySelector("#humidityResult");
- console.log(humidityResultnow);
- humidityResultnow.innerHTML = `${humidityNow}%`;
-
- windDescriptionNow = document.querySelector("#windDescription");
- windDescriptionNow.innerHTML =  `${windNow}km/hr`
-
- timeDescriptionNow = document.querySelector("#weatherTimeNow");
- timeDescriptionNow.innerHTML = `${date.getDay} `
-});
-
+    
+    iconElement = document.querySelector("#iconTemperature");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" alt="" class="icon">`;
 
 }
-);
+
+function formatDate(date) {
+   
+   hours = date.getHours();
+   minutes = date.getMinutes();
+
+   days = ["Sunday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"];
+day = days[date.getDay()];
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+return `${day} ${hours}:${minutes}`;
+}
+
+
+function searchCity(city) {
+  let apikey =   "7d3fo7t42ac4a88b5f1b6383fbbe2b0b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then(updateWeather);
+
+}
+
+
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+
+    seachFormInput = document.querySelector("#searchFormInputes");
+    console.log(seachFormInput.value);
+
+    
+    
+    searchCity(seachFormInput.value);
+}
+
+
+searchFormElement = document.querySelector("#seachForm-identity");
+console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
