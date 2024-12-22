60412edad2c27c123b4302205b1e873fdee0585c
ISIOMA123123 <ubanisuccess26@gmail.com>
2024-12-22 15:38:15 +0100

    updated index.js

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ function updateWeather(response) {
     iconElement = document.querySelector("#iconTemperature");
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" alt="" class="icon">`;
 
+    getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -67,7 +68,17 @@ function handleSearchSubmit(event) {
     searchCity(seachFormInput.value);
 }
 
-function displayforecast() {
+function getForecast(city) {
+ let apikey = "7d3fo7t42ac4a88b5f1b6383fbbe2b0b";
+ let apiUrl = 
+ `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayforecast);
+
+}
+
+function displayforecast(response) {
+  console.log(response);
  
 
 let days = ["Tues", "Wed", "Thu", "Fri", "Sat"];
@@ -96,5 +107,6 @@ searchFormElement = document.querySelector("#seachForm-identity");
 console.log(searchFormElement);
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Lisbon");
-displayforecast();
+searchCity("Paris");
+
+
